957ffd563e64c2a70c5fe82e628c3751b0b0d7ae
Kareem Yacoub <kyacou2@uic.edu>
2019-04-15 10:30:14 -0500

    started adding cards to page

--- projectData.js
+++ projectData.js
@@ -1,8 +1,8 @@
-var projectData = {
+var projectData = [{
     title: 'My Project',
     description: 'A quick description of the what the project does.',
     technologies: ['Programming language', 'Framework', 'etc'],
     date: 'MM/DD/YYYY',
     project_url: 'karyacoub.github.io/projectname',
     source_url: 'github.com/karyacoub/projectname'
-}
+}]
