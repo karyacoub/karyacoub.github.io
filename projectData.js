3a8872996f327f7ffbc9c324b9dc61f192fdab83
Kareem Yacoub <kyacou2@uic.edu>
2019-04-15 10:38:59 -0500

    added sample project links

--- projectData.js
+++ projectData.js
@@ -3,6 +3,6 @@ var projectData = [{
     description: 'A quick description of the what the project does.',
     technologies: ['Programming language', 'Framework', 'etc'],
     date: 'MM/DD/YYYY',
-    project_url: 'karyacoub.github.io/projectname',
-    source_url: 'github.com/karyacoub/projectname'
+    project_url: 'https://karyacoub.github.io/it202-project1/',
+    source_url: 'https://github.com/karyacoub/it202-project1'
 }]
